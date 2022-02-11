b14df8fb0792fb04d8e4ec8fcd8c8373dad9d755
gmarcha <gmarcha@github.com>
2022-02-11 12:38:21 -0500

    update incs: add utils header (printErr and printGrid)

---
+++ incs/utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H
+# define UTILS_H
+
+#include <stdio.h>
+#include <math.h>
+#include "defines.h"
+#include "types.h"
+
+void printErr( const string errMsg );
+// Utility function to print formatted error.
+
+void printGrid( gridPtr grid );
+// Utility function to print sudoky grid.
+
+#endif // UTILS_H
